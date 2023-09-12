27893e85c2f0f38e8c2f48aa92c9d31e90a8193d
Arya Lesmana <arya.lesmana21@ui.ac.id>
2023-09-12 21:50:39 +0700

    Menambahkan test untuk model

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase, Client
+from .models import Item
 
-# Create your tests here.
+class ItemModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Item.objects.create(name='Blue Potion', amount=5, description='Potion for test purpose only',
+                            effect='Give your test result', value=100)
+    
+    def test_name_label(self):
+        item = Item.objects.get(id=1)
+        expected_output = item._meta.get_field('name').verbose_name
+        self.assertEqual(expected_output, 'name')
+
+    def test_amount_label(self):
+        item = Item.objects.get(id=1)
+        expected_output = item._meta.get_field('amount').verbose_name
+        self.assertEqual(expected_output, 'amount')
+    
+    def test_description_label(self):
+        item = Item.objects.get(id=1)
+        expected_output = item._meta.get_field('description').verbose_name
+        self.assertEqual(expected_output, 'description')
+    
+    def test_effect_label(self):
+        item = Item.objects.get(id=1)
+        expected_output = item._meta.get_field('effect').verbose_name
+        self.assertEqual(expected_output, 'effect')
+    
+    def test_value_label(self):
+        item = Item.objects.get(id=1)
+        expected_output = item._meta.get_field('value').verbose_name
+        self.assertEqual(expected_output, 'value')
+    
+    def test_name_max_length(self):
+        item = Item.objects.get(id=1)
+        expected_length = item._meta.get_field('name').max_length
+        self.assertEqual(expected_length, 100)
